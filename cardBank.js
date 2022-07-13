ac77aac33d69ab10fef1250143eb3d54fadfc903
d-rocham <d_rocham@outlook.com>
2022-07-13 12:05:59 -0500

    Added no-unused-vars rule

--- cardBank.js
+++ cardBank.js
@@ -2,30 +2,30 @@ const generateCard = require("./card");
 
 require("./card")();
 
-const cardBank = (cards) => {
-  const CARDS = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J"];
+const cardBank = () => {
+	const CARDS = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J"];
 
-  // Immediately Invoked Function Expression. Only one deck is required
-  const cardsDeck = (() => {
-    let deck = CARDS.map((card) => generateCard(card));
+	// Immediately Invoked Function Expression. Only one deck is required
+	const cardsDeck = (() => {
+		let deck = CARDS.map((card) => generateCard(card));
 
-    return deck;
-  })();
+		return deck;
+	})();
 
-  const selectRandomCard = () => {
-    let randomIndex = Math.floor(Math.random() * cardsDeck.length);
-    let randomCard = cardsDeck[randomIndex];
+	const selectRandomCard = () => {
+		let randomIndex = Math.floor(Math.random() * cardsDeck.length);
+		let randomCard = cardsDeck[randomIndex];
 
-    // Select a random card & remove it from the deck.
-    randomCard = cardsDeck.splice(randomIndex, 1);
+		// Select a random card & remove it from the deck.
+		randomCard = cardsDeck.splice(randomIndex, 1);
 
-    return randomCard;
-  };
+		return randomCard;
+	};
 
-  return {
-    cardsDeck,
-    selectRandomCard,
-  };
+	return {
+		cardsDeck,
+		selectRandomCard,
+	};
 };
 
 module.exports = cardBank;
