ef2dd0d6856c033d2f9426bfad1a39e8f507de1a
d-rocham <d_rocham@outlook.com>
2022-07-14 12:50:50 -0500

    Fixed bug on finishGame

--- src/gameplay/finishGame.js
+++ src/gameplay/finishGame.js
@@ -1,12 +1,12 @@
 import { renderGameResults } from "../renderGame/renderGame";
 
 const finishGame = (winner) => {
-	if (winner === "player") {
-		renderGameResults("Congratulations: YOU WON!");
-		return;
-	}
+	const endMessage =
+		winner === "player"
+			? "Congratulations: YOU WON!"
+			: "Too bad. YOU LOST :(";
 
-	renderGameResults("Too bad. YOU LOST :(");
+	renderGameResults(endMessage);
 };
 
 export default finishGame;
